2086eed73ec307925cb0ef570dbf407d610c4143
Karthik Mohan <>
2020-08-18 19:06:38 -0500

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBqyyHI3BFJhQmom09bou9Vqmjd3ZOrhow",
+    authDomain: "whats-app-clone-87200.firebaseapp.com",
+    databaseURL: "https://whats-app-clone-87200.firebaseio.com",
+    projectId: "whats-app-clone-87200",
+    storageBucket: "whats-app-clone-87200.appspot.com",
+    messagingSenderId: "273924253276",
+    appId: "1:273924253276:web:113a6e75dcd32af6bc2899",
+    measurementId: "G-RSZK9RLQFX"
+  };
